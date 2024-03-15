972597511dbcc993163b25917565448ec9df2aa7
Nicolas Körner <nkoerner93@gmail.com>
2024-03-15 04:12:59 +0100

    Random change for new redeployment to change vercel server location

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,18 @@
 import Header from "@/components/ui/Header";
+import HeroSection from "@/components/ui/HeroSection";
 
 export default async function Dashboard() {
-  return <Header />;
+  return (
+    <div className="relative flex min-h-screen flex-col bg-background">
+      <Header />
+      <HeroSection
+        title="Discover new Animes"
+        subtitle="Check out the list of currently running animes in this season."
+        button1="Get Started"
+        button1link="/"
+        button2="Favorites"
+        button2link="https://github.com/nkoerner93/spark"
+      />
+    </div>
+  );
 }
