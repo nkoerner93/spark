a920e1204f7c5d29e8c0d14c4d71d6c64e6ae529
Nicolas Körner <nkoerner93@gmail.com>
2024-03-25 15:20:10 +0100

    Update testing

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -6,14 +6,6 @@ describe("Test Login", () => {
     cy.reload();
     LoginPage.visit();
   });
-
-  it("should successfully log in with correct credentials", () => {
-    LoginPage.fillUsername(Cypress.env("CYPRESS_LOGIN_USERNAME_ADMIN"));
-    LoginPage.fillPassword(Cypress.env("CYPRESS_LOGIN_PASSWORD_ADMIN"));
-    LoginPage.submitLoginForm();
-    LoginPage.verifyLoginSuccess();
-  });
-
   it("should display error message with incorrect credentials", () => {
     LoginPage.fillUsername("invalid_username@randomxPl.com");
     LoginPage.fillPassword("invalid_password@randomxPl.com");
