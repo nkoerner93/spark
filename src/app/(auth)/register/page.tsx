10685a3685a783185138515b6a397819e13e2dc6
Nicolas Körner <nkoerner93@gmail.com>
2024-03-25 14:33:47 +0100

    Add use client to register/loginpage

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -1,7 +1,9 @@
+"use client";
+
 import AuthLayout from "@/app/(auth)/AuthLayout";
 import RegisterForm from "@/app/(auth)/register/components/RegisterForm";
 
-export default async function RegisterPage() {
+export default function RegisterPage() {
   return (
     <AuthLayout>
       <RegisterForm />
