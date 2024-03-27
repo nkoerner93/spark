d1ccec4e196b5dbaee51e5f4fbbd753cfd5f1a09
Nicolas Körner <nkoerner93@gmail.com>
2024-03-27 12:23:07 +0100

    patch pnpm package for react-slick slider.js

---
+++ src/components/Poe_CurrencyMapCalculator.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+interface Poe_CurrencyMapCalculatorProps {}
+
+const Poe_CurrencyMapCalculator: FC<Poe_CurrencyMapCalculatorProps> = ({}) => {
+  return <div>Poe_CurrencyMapCalculator</div>;
+};
+
+export default Poe_CurrencyMapCalculator;
