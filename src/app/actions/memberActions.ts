8eb0e87c23ba3e1f75105820c69cf429beb7931f
Nicolas Körner <nkoerner93@gmail.com>
2024-08-11 21:32:37 +0200

    Update getallUsers function to accept boolean args for public/private users, reformat users page with shadcn table.

--- src/app/actions/memberActions.ts
+++ src/app/actions/memberActions.ts
@@ -2,12 +2,15 @@ import prisma from "@/app/prisma";
 import { getSession } from "./actions";
 import { Users } from "@prisma/client";
 
-export async function getAllUsers(): Promise<Users[]> {
+//
+export async function getPublicUsers(boolean: boolean): Promise<Users[]> {
   // Validate Session
   const session = await getSession();
   if (!session.userId) throw new Error("Not authenticated");
 
-  const allusers = await prisma.users.findMany({ where: { isPublic: true } });
+  const allusers = await prisma.users.findMany({
+    where: { isPublic: boolean },
+  });
 
   if (!allusers) {
     throw new Error("No users found.");
--- src/app/dashboard/users/page.tsx
+++ src/app/dashboard/users/page.tsx
@@ -1,17 +1,41 @@
-import { getAllUsers } from "@/app/actions/memberActions";
+import { getPublicUsers } from "@/app/actions/memberActions";
+import HeroSection from "@/components/ui/HeroSection";
+import {
+  Table,
+  TableHeader,
+  TableRow,
+  TableHead,
+  TableCell,
+  TableBody,
+} from "@/components/ui/shad-cn/table";
 import React from "react";
 
 const UserDashboard = async () => {
   // Await the asynchronous function
-  const allusers = await getAllUsers();
+  const allusers = await getPublicUsers(false);
   console.log(allusers);
 
   return (
     <div>
-      {allusers.map((user) => (
-        // Return the JSX element
-        <span key={user.id}>{user.username}</span>
-      ))}
+      <HeroSection title="Users" subtitle={`List of all our users.`} />
+      <div className="mx-auto flex max-w-screen-lg flex-row justify-center gap-8">
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="w-[100px]">ID</TableHead>
+              <TableHead className="text-right">Username</TableHead>
+            </TableRow>
+            {allusers.map((user, index) => (
+              // Return the JSX element
+              <TableRow key={user.id}>
+                <TableCell className="font-medium">{index + 1}</TableCell>
+                <TableCell className="text-right">{user.username}</TableCell>
+              </TableRow>
+            ))}
+          </TableHeader>
+          <TableBody></TableBody>
+        </Table>
+      </div>
     </div>
   );
 };
