c44d43158463e13a4a42f857bdccadb762e5d3ae
Nicolas Körner <nkoerner93@gmail.com>
2024-08-13 00:08:03 +0200

    Add ReturnButton to Highest Rated Animes.

--- src/app/dashboard/anime/ranking/RankingTypeSelector.tsx
+++ src/app/dashboard/anime/ranking/RankingTypeSelector.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import ReturnButton from "@/components/ui/ReturnButton";
 import { Button } from "@/components/ui/shad-cn/button";
 import { useRouter } from "next/navigation";
 
@@ -15,19 +16,22 @@ export default function RankingTypeSelector({
   };
 
   return (
-    <div className="flex flex-row gap-2 pb-1">
-      <Button
-        onClick={() => handleRankingTypeChange("all")}
-        variant={currentType === "all" ? "default" : "outline"}
-      >
-        All
-      </Button>
-      <Button
-        onClick={() => handleRankingTypeChange("airing")}
-        variant={currentType === "airing" ? "default" : "outline"}
-      >
-        Airing
-      </Button>
+    <div className="flex flex-row justify-between gap-2 pb-1 md:px-2">
+      <ReturnButton />
+      <div className="flex flex-row gap-2">
+        <Button
+          onClick={() => handleRankingTypeChange("all")}
+          variant={currentType === "all" ? "default" : "outline"}
+        >
+          All
+        </Button>
+        <Button
+          onClick={() => handleRankingTypeChange("airing")}
+          variant={currentType === "airing" ? "default" : "outline"}
+        >
+          Airing
+        </Button>
+      </div>
     </div>
   );
 }
