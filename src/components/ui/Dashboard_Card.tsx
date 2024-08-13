2065d213d5602215accf332dc83789dc44fdbae0
Nicolas Körner <nkoerner93@gmail.com>
2024-08-13 16:26:03 +0200

    Revert next/image and move DashboardCard Enums to const folder.

--- src/components/ui/Dashboard_Card.tsx
+++ src/components/ui/Dashboard_Card.tsx
@@ -7,29 +7,10 @@ import {
 } from "@/components/ui/shad-cn/card";
 import { getCurrentSeason } from "@/lib/utils/utils";
 import Link from "next/link";
-import Image from "next/image";
-
-// Define enum for card categories
-export enum CardCategory {
-  Anime = "TV Series",
-  Productivity = "Productivity",
-  Gaming = "Gaming",
-  SeasonalAnime = "Seasonal Animes",
-  Ranking = "Highest Rated",
-  PathOfExile = "Path of Exile",
-  MapCalculator = "POE Map and Profit Calculator",
-}
-
-// Define enum for card descriptions
-export enum CardDescriptionEnum {
-  Anime = "Discover the latest television or anime series.",
-  Productivity = "Boost your productivity with planning tools, task tracking and more.",
-  Gaming = "Improve your gameplay with useful gaming tools like POE Map Tracking, POE Currency Coverter and more.",
-  SeasonalAnime = "Discover new seasonal animes from",
-  Ranking = "Check out the highest rated animes this season or of all time.",
-  PathOfExile = "Action-RPG",
-  MapCalculator = "Calculate your profits per Map or over multiple sessions",
-}
+import {
+  CardCategory,
+  CardDescriptionEnum,
+} from "src/constants/dashboardCardEnums";
 
 interface DashboardCardProps {
   title: CardCategory;
@@ -86,13 +67,10 @@ export const DashboardCard: FC<DashboardCardProps> = ({ title, link }) => {
       <Card className="rounded-lg drop-shadow-md md:w-[325px] lg:w-[400px] xl:w-[450px]">
         <CardHeader className="p-0">
           <Link href={link}>
-            <Image
+            <img
               className="h-[350px] w-full rounded-t-lg object-cover opacity-100 contrast-[108%] transition duration-200 hover:cursor-pointer hover:contrast-125"
               src={getImagePath(title)} // Use getImagePath function
               alt={`${title} Card`}
-              width={0}
-              height={0}
-              sizes="100vw"
             />
           </Link>
           <div className="flex flex-col px-6 pb-5 pt-2">
@@ -104,3 +82,4 @@ export const DashboardCard: FC<DashboardCardProps> = ({ title, link }) => {
     </div>
   );
 };
+export { CardCategory };
