5a0c52ecf4b606d8b8d7f4e82824da8f0ad75cf9
Nicolas Körner <nkoerner93@gmail.com>
2024-08-16 21:56:37 +0200

    Add a database check if the series already exists in the db for the specific user. Add conditionals & toast feedback accordingly.

--- src/app/actions/tvSeriesActions.ts
+++ src/app/actions/tvSeriesActions.ts
@@ -3,6 +3,7 @@ import { redirect } from "next/navigation";
 import prisma from "../prisma";
 import { getSession } from "./actions";
 import { Anime } from "src/types/types";
+import { SeriesType } from "@prisma/client";
 
 // ADD SERIES TO USER FAVORITES
 export async function createTVSeries(anime: Anime) {
@@ -10,16 +11,30 @@ export async function createTVSeries(anime: Anime) {
   if (!session || !session.userId) return redirect("/login");
 
   try {
+    // Check if the anime already exists for this user
+    const existingAnime = await prisma.subscribed_TVSeries.findFirst({
+      where: {
+        userId: session.userId,
+        title: anime.title,
+        type: SeriesType.ANIME,
+      },
+    });
+
+    if (existingAnime) {
+      // If the anime already exists, return a specific object
+      return { success: false, reason: "already_exists" };
+    }
+
     const result = await prisma.subscribed_TVSeries.create({
       data: {
         userId: session.userId,
         title: anime.title,
         imgurl: anime.main_picture.medium,
-        type: "ANIME",
+        type: SeriesType.ANIME,
       },
     });
 
-    return result;
+    return { success: true, data: result };
   } catch (error) {
     console.error("Error adding series to favorites:", error);
     throw error; // Re-throw the error for further handling in the component
--- src/components/ui/AddSeriesToFavorites.tsx
+++ src/components/ui/AddSeriesToFavorites.tsx
@@ -16,11 +16,26 @@ const AddSeriesToFavorites = ({ series }: { series: Anime_Data_Seasonal }) => {
     try {
       setIsAddingToFavorites(true);
       // Add the series to the user's favorites in the database
-      await createTVSeries(series.node);
+      const result = await createTVSeries(series.node);
+
+      if (!result.success) {
+        if (result.reason === "already_exists") {
+          toast({
+            title: "Already in your list!",
+            description: `The series ${series.node.title} is already in your favorites.`,
+            variant: "destructive",
+          });
+        }
+        setIsAddingToFavorites(false);
+        return;
+      }
+
+      // Series successfully added
       toast({
         title: "Success!",
         description: `The series ${series.node.title} has been added to your favorites.`,
       });
+      // Set States to change icons
       setIsAddingToFavorites(false);
       setIsSuccessfull(true);
       setTimeout(() => {
@@ -28,12 +43,14 @@ const AddSeriesToFavorites = ({ series }: { series: Anime_Data_Seasonal }) => {
         setIsAddingToFavorites(false);
       }, 2500);
     } catch (error) {
+      // Error adding series
       toast({
         title: "Error!",
         description: `There was an error adding ${series.node.title} to your favorites. Please try again.`,
         variant: "destructive",
       });
       console.error("Error adding series to favorites:", error);
+      setIsAddingToFavorites(false);
     }
   };
 
