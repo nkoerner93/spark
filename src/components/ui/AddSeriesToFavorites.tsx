ade18c26a701f7731aecd6dc24b668fb14a5b7a9
Nicolas Körner <nkoerner93@gmail.com>
2024-08-16 19:56:33 +0200

    Add tooltips to plus button for visual feedback to save the series to favorites.

--- src/components/ui/AddSeriesToFavorites.tsx
+++ src/components/ui/AddSeriesToFavorites.tsx
@@ -1,21 +1,32 @@
 "use client";
 import { createTVSeries } from "@/app/actions/tvSeriesActions";
-import { Plus } from "lucide-react";
-import React from "react";
+import { Check, Loader2, Plus } from "lucide-react";
+import React, { useState } from "react";
 import { Anime_Data_Seasonal } from "src/types/types";
 import { useToast } from "@/components/ui/shad-cn/use-toast";
+import { Tooltip, TooltipContent } from "./shad-cn/tooltip";
+import { TooltipProvider, TooltipTrigger } from "@radix-ui/react-tooltip";
 
 const AddSeriesToFavorites = ({ series }: { series: Anime_Data_Seasonal }) => {
   const { toast } = useToast();
+  const [isAddingToFavorites, setIsAddingToFavorites] = useState(false);
+  const [isSuccessfull, setIsSuccessfull] = useState(false);
 
   const handleAddToFavorites = async (series: Anime_Data_Seasonal) => {
     try {
+      setIsAddingToFavorites(true);
       // Add the series to the user's favorites in the database
       await createTVSeries(series.node);
       toast({
         title: "Success!",
         description: `The series ${series.node.title} has been added to your favorites.`,
       });
+      setIsAddingToFavorites(false);
+      setIsSuccessfull(true);
+      setTimeout(() => {
+        setIsSuccessfull(false);
+        setIsAddingToFavorites(false);
+      }, 2500);
     } catch (error) {
       toast({
         title: "Error!",
@@ -28,10 +39,23 @@ const AddSeriesToFavorites = ({ series }: { series: Anime_Data_Seasonal }) => {
 
   return (
     <div>
-      <Plus
-        onClick={() => handleAddToFavorites(series)}
-        className="h-14 w-14 text-2xl text-black hover:cursor-pointer hover:text-yellow-400"
-      />
+      {isAddingToFavorites ? (
+        <Loader2 className="h-14 w-14 animate-spin text-2xl text-black hover:cursor-pointer" />
+      ) : isSuccessfull ? (
+        <Check className="h-14 w-14 text-2xl text-black hover:cursor-pointer" />
+      ) : (
+        <TooltipProvider>
+          <Tooltip delayDuration={0}>
+            <TooltipTrigger>
+              <Plus
+                onClick={() => handleAddToFavorites(series)}
+                className="h-14 w-14 text-2xl text-black hover:cursor-pointer"
+              />
+            </TooltipTrigger>
+            <TooltipContent>Save to favorites</TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
+      )}
     </div>
   );
 };
