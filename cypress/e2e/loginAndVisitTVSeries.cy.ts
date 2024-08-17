f34db42a80717d263531f51d3d9a641994a6aba3
Nicolas Körner <nkoerner93@gmail.com>
2024-08-17 21:27:53 +0200

    cy test

--- cypress/e2e/loginAndVisitTVSeries.cy.ts
+++ cypress/e2e/loginAndVisitTVSeries.cy.ts
@@ -8,10 +8,11 @@ describe("Test Login", () => {
       Cypress.env("CYPRESS_LOGIN_PASSWORD_ADMIN"),
     );
     cy.get('[data-login="button-submit"]').click();
-    cy.wait(2000);
+    cy.wait(1500);
+    it("should succesfully be redirected to /dashboard");
     cy.url().should("include", "/dashboard");
-    cy.wait(2000);
-    cy.visit("http://localhost:3000/dashboard/anime");
+    cy.wait(1500);
+    cy.visit("dashboard/anime");
     cy.get('[data-card="card-single"]').should("be.visible");
   });
 });
