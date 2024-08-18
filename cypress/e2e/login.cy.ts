d090301392552a0678f4d60696a3d8fc424c59cb
Nicolas Körner <nkoerner93@gmail.com>
2024-08-18 23:49:16 +0200

    change timings

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -27,6 +27,7 @@ describe("Go to the login page and test a successfull & incorrect user login.", 
       "invalid_password@randomxPl.com",
     );
     cy.get('[data-login="button-submit"]').click();
+    cy.wait(2000);
     const toast = cy.get('[data-toast="toast-title"]');
     toast.contains("Error");
   });
