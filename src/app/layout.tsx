4abbb900be852e71ae382fae936324cc34686842
Nicolas Körner <nkoerner93@gmail.com>
2024-08-20 11:48:34 +0200

    wrap children in div to fix hydration error.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          {children}
+          <div>{children}</div>
         </ThemeProvider>
       </body>
     </html>
